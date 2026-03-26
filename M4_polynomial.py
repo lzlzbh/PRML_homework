ad6df67b94c275031c3919f35c32684664189bc3
lzlzbh <1759950806@qq.com>
2026-03-26 20:04:53 +0800

    Update M4_polynomial.py

--- M4_polynomial.py
+++ M4_polynomial.py
@@ -2,14 +2,11 @@ import torch
 import pandas as pd
 train_df=pd.read_csv(r"E:\desktop_wj\PRML_HOMEWORK\HOMEWORK1\DATA\Data4Regression - Training Data.csv")
 test_df=pd.read_csv(r"E:\desktop_wj\PRML_HOMEWORK\HOMEWORK1\DATA\Data4Regression - Test Data.csv")
-x_train_col='x_new' if 'x_new' in train_df.columns else 'x'
-y_train_col='y_new_complex' if 'y_new_complex' in train_df.columns else 'y_complex'
-x_test_col='x_new' if 'x_new' in test_df.columns else 'x'
-y_test_col='y_new_complex' if 'y_new_complex' in test_df.columns else 'y_complex'
-x_train=torch.tensor(train_df[x_train_col].values, dtype=torch.float32)
-y_train=torch.tensor(train_df[y_train_col].values, dtype=torch.float32)
-x_test=torch.tensor(test_df[x_test_col].values, dtype=torch.float32)
-y_test=torch.tensor(test_df[y_test_col].values, dtype=torch.float32)
+x_train = torch.tensor(train_df['x'].values, dtype=torch.float32)
+y_train = torch.tensor(train_df['y_complex'].values, dtype=torch.float32)
+x_test = torch.tensor(test_df['x_new'].values, dtype=torch.float32)
+y_test = torch.tensor(test_df['y_new_complex'].values, dtype=torch.float32)
+#从csv取出来数据，并整成pytorch张量
 def evaluate(truth, pred):
     rmse=torch.sqrt(torch.mean((truth-pred)**2)).item()
     ss_res=torch.sum((truth-pred)**2)
